5c280f26e89b9bcc378b753beb4070f9c3b9f777
Yurii Tolochko <ytolochko@gmail.com>
2018-07-12 01:03:11 +0200

    added visualization of events on a map

---
+++ drawing_map.R
@@ -0,0 +1,51 @@
+install.packages('ggmap')
+install.packages('rworldmap')
+
+
+library(mapproj)
+map <- get_map(location = 'Europe', zoom = 4, maptype = 'hybrid')
+ggmap(map)
+
+library(rworldmap)
+newmap <- getMap(resolution = "low")
+plot(newmap)
+
+
+m<-get_map(location=c(lon=24, lat=1), zoom=5, color="bw")
+ggmap(m)
+
+
+
+aid <- read.csv('C:\\Users\\Yurii\\Desktop\\Konstanz stuff\\Diploma\\Data\\aid_data\\data\\level_1a.csv')
+aid$log_commitments <- log(aid$even_split_commitments)
+aid$transactions_start_year <-  as.factor(aid$transactions_start_year)
+
+conflict <- aid <- read.csv('C:\\Users\\Yurii\\Desktop\\Konstanz stuff\\Diploma\\Data\\conflict_data\\DRC_2.csv')
+
+names(conflict)
+
+
+install.packages('maptools')
+library('maptools')
+library(ggmap)
+
+shpfile <- 'C:\\Users\\Yurii\\Desktop\\Konstanz stuff\\Diploma\\Data\\gadm36_COD_shp\\gadm36_COD_1.shp'
+sh <- readShapePoly(shpfile)
+plot(sh)
+
+points <- fortify(sh)
+drc <- qmap('DRC', zoom = 5)
+
+drc + geom_polygon(aes(x = long, y = lat, group=group), data = points, fill = 'white') +
+  geom_polygon(aes(x=long,y=lat, group=group), data=points, color='black', fill=NA) + 
+  geom_point(aes(x = longitude, y = latitude, size = log_commitments), data = aid)
+
+drc + geom_polygon(aes(x = long, y = lat, group=group), data = points, fill = 'white') +
+  geom_polygon(aes(x=long,y=lat, group=group), data=points, color='black', fill=NA) + 
+  geom_point(aes(x = longitude, y = latitude, size = even_split_commitments, color = transactions_start_year), data = aid) +
+  scale_size(range = c(1,12))
+
+drc + geom_polygon(aes(x = long, y = lat, group=group), data = points, fill = 'white') +
+  geom_polygon(aes(x=long,y=lat, group=group), data=points, color='black', fill=NA) + 
+  geom_point(aes(x = longitude, y = latitude, size = best_est, color = year), data = conflict) +
+  scale_size(range = c(1,12))
